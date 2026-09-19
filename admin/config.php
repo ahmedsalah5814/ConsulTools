5fbbbd1b2d687bce417bf29f666734f340ea5278
ahmedsalah5814 <ahmedsalah5814@gmail.com>
2026-09-19 10:12:28 +0300

    Add files via upload

---
+++ admin/config.php
@@ -0,0 +1,5 @@
+<?php
+// غيّر الهاش قبل النشر. كلمة مرور مؤقتة: ChangeMeNow123!
+// توليد هاش جديد: php -r "echo password_hash('YOUR_PASSWORD', PASSWORD_DEFAULT);"
+define('ADMIN_PASSWORD_HASH', '$2y$12$WHi/QW7.fBcyjtYZ.IUlSup4GaP370kdMc2sdOwBxdiLZQb1Bv5U.');
+?>
